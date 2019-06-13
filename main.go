9275ffd71f2dee089121ab9f37cd864980555b7d
muskankhedia <muskan.khedia2000@gmail.com>
2019-06-14 03:27:37 +0530

    some changes

--- main.go
+++ main.go
@@ -27,6 +27,8 @@ type jsonResponse struct {
 
 func fetchData(w http.ResponseWriter, r *http.Request) {
 
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
   r.ParseForm()
 
   request := r.FormValue("commodity")
